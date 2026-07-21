aaf8510d7262149a19956c5d6745829cf24bc6a6
AKToran <aktoran60@gmail.com>
2026-07-21 14:48:01 +0600

    sendResponse utility and refactor registerUser

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -2,22 +2,13 @@ import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status";
 import { userService } from "./user.service";
 import { catchAsync } from "../../utils/catchAsync";
-
-// const registerUser = async (req: Request, res: Response) => {
-//   try {
-//     const user = await userService.registerUserInDB(req.body);
-//     res.status(httpStatus.CREATED).json({
-//       success: true,
-//       message: "User registered successfully",
-//       data: user,
-//     });
-//   } catch (error) {}
-// };
+import { sendResponse } from "../../utils/sendResponse";
 
 const registerUser = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
   const user = await userService.registerUserInDB(req.body);
-  res.status(httpStatus.CREATED).json({
+  sendResponse(res, {
     success: true,
+    statusCode: httpStatus.CREATED,
     message: "User registered successfully",
     data: user,
   });
