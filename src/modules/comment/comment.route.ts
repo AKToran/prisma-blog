c106a0405f8a0cb1067d42456d6183a80884f00c
AKToran <aktoran60@gmail.com>
2026-07-23 14:23:02 +0600

    add comment module with controller, service, interface, and route setup

--- src/modules/comment/comment.route.ts
+++ src/modules/comment/comment.route.ts
@@ -1,1 +1,43 @@
+import { Router } from "express";
+import { Role } from "../../../generated/prisma/enums";
+import auth from "../../middleware/auth";
+import { commentController } from "./comment.controller";
 
+const router = Router();
+
+router.post( 
+    "/",
+    auth(Role.user, Role.admin),
+    commentController.createComment
+);
+
+router.get(
+    "/author/:authorId",
+    commentController.getCommentByAuthorId
+);
+
+router.get(
+    "/:commentId",
+    commentController.getCommentByCommentId
+);
+
+router.patch(
+    "/:commentId",
+    auth(Role.user, Role.admin),
+    commentController.updateComment
+);
+
+router.delete(
+    "/:commentId",
+    auth(Role.user, Role.admin),
+    commentController.deleteComment
+);
+
+router.put(
+    "/:commentId/moderate",
+    auth(Role.admin),
+    commentController.moderateComment
+);
+
+
+export const commentRoute = router;
