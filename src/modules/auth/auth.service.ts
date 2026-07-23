5cab97701748aea2d70f80a5716083d8cbb3400f
AKToran <aktoran60@gmail.com>
2026-07-23 12:45:51 +0600

    implement refresh token endpoint

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,10 +1,10 @@
 import bcrypt from "bcryptjs";
 import { prisma } from "../../lib/prisma";
 import { ILoginUser } from "./auth.interface";
-import jwt, { SignOptions } from "jsonwebtoken";
 import config from "../../config";
 import { jwtUtils } from "../../utils/jwt";
 import { ActiveStatus } from "../../../generated/prisma/client";
+import { JwtPayload } from "jsonwebtoken";
 
 const loginUserService = async (payload: ILoginUser) => {
   const { email, password } = payload;
@@ -45,6 +45,41 @@ const loginUserService = async (payload: ILoginUser) => {
   return { accessToken, refreshToken };
 };
 
+const refreshToken = async (refreshToken: string) => {
+  const verifiedRefreshToken = jwtUtils.verifyToken(
+    refreshToken,
+    config.jwt_refresh_secret,
+  );
+
+  const { id } = verifiedRefreshToken as JwtPayload;
+
+  const user = await prisma.user.findFirstOrThrow({
+    where: {
+      id,
+    },
+  });
+
+  if (user.activeStatus === "blocked") {
+    throw new Error("User is blocked. Contact support.");
+  }
+
+  const jwtPayload = {
+    id,
+    name: user.name,
+    email: user.email,
+    role: user.role,
+  };
+
+  const accessToken = jwtUtils.createToken(
+    jwtPayload,
+    config.jwt_access_secret,
+    config.jwt_access_expiration,
+  );
+
+  return accessToken;
+};
+
 export const authService = {
   loginUserService,
+  refreshToken,
 };
